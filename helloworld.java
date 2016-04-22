4f7d88ade3d7cf0e8989a258efb793093a8f30ae
Mohammad Shahidul Islam <shahedshahidul2@gmail.com>
2016-04-21 17:06:42 -0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,7 +2,7 @@
 public class helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("This is hello world programm to test in jenkins");
+		System.out.println("This is hello world programm to test in jenkins from gitHub");
 
 	}
 
